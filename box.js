942f904bf4e3e5d0759966279502d7eed9d2b1a4
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2022-03-14 17:31:52 -0700

    Update box.js

--- box.js
+++ box.js
@@ -32,6 +32,18 @@
  onCustomWidgetAfterUpdate(changedProperties) {
  if ("color" in changedProperties) {
  this.style["background-color"] = changedProperties["color"];
+var table = this._shadowRoot.getElementById("myTable");
+
+// Create an empty <tr> element and add it to the 1st position of the table:
+var row = table.insertRow(0);
+
+// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+
+// Add some text to the new cells:
+cell1.innerHTML = "NEW CELL1";
+cell2.innerHTML = "NEW CELL2";  
  }
  if ("opacity" in changedProperties) {
  this.style["opacity"] = changedProperties["opacity"];
