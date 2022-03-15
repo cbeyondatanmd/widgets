7f89b190595dd5ab968db5ed47333d1eedbba9e2
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2022-03-14 19:07:58 -0700

    Update box.js

--- box.js
+++ box.js
@@ -1,25 +1,6 @@
 (function() {
  let template = document.createElement("template");
  template.innerHTML = `
- 
- <script>
-function myFunction(newCaption, newHref) {
-var table = this.shadowRoot.getElementById("myTable");
-
-// Create an empty <tr> element and add it to the 1st position of the table:
-var row = table.insertRow(0);
-
-// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-
-// Add some text to the new cells:
-cell1.innerHTML = newCaption;
-cell2.innerHTML = newHref;  
-}
-document.getElementById("demo").innerHTML = myFunction(4, 3);
-</script>
-
 <table id="myTable">
 
 </table>
