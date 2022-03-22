dcc71b80e28a13a79b57e51a8b498ad33dd4601d
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2022-03-22 11:12:36 -0700

    Update box.js

--- box.js
+++ box.js
@@ -18,6 +18,18 @@
     .hoverTable tr:hover {
           background-color: rgb(40, 56, 72);
     }
+a:link, a:visited {
+  background-color: #f44336;
+  color: white;
+  padding: 15px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active {
+  background-color: red;
+}
 </style> 
 <table id="myTable" class="hoverTable">
 
