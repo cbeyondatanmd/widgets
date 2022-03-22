88eedc9e2f9b233c3fd2b8116c1d99d08fe872c5
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2022-03-22 10:28:45 -0700

    Update box.js

--- box.js
+++ box.js
@@ -1,6 +1,6 @@
 (function() {
- let template = document.createElement("template");
- template.innerHTML = `
+	let template = document.createElement("template");
+	template.innerHTML = `
 <style style="text/css">
   	.hoverTable{
 		width:100%; 
@@ -27,37 +27,63 @@
  `;
 
 
- class Box extends HTMLElement {
- constructor() {
- super();
- let shadowRoot = this.attachShadow({mode: "open"});
- shadowRoot.appendChild(template.content.cloneNode(true));
- this.addEventListener("click", event => {
- var event = new Event("onClick");
- this.dispatchEvent(event);
- });
- this._props = {};
- }
- onCustomWidgetBeforeUpdate(changedProperties) {
- this._props = { ...this._props, ...changedProperties };
- }
- onCustomWidgetAfterUpdate(changedProperties) {
- if ("addUrl" in changedProperties) {
-    var table = this.shadowRoot.getElementById("myTable");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    cell1.innerHTML=  '<a href="'+ changedProperties["addUrl"].split('|')[1] +'">'+ changedProperties["addUrl"].split('|')[0] +'</a>';
-cell1.addEventListener("click",() => {
-	this.selectedItem = changedProperties["addUrl"].split('|')[0];
-});	 
- }  
- if ("color" in changedProperties) {
- this.style["background-color"] = changedProperties["color"];
- }
- if ("opacity" in changedProperties) {
- this.style["opacity"] = changedProperties["opacity"];
- }
- }
- }
-customElements.define("com-sample-box", Box);
+	class Box extends HTMLElement {
+		constructor() {
+			super();
+			let shadowRoot = this.attachShadow({
+				mode: "open"
+			});
+			shadowRoot.appendChild(template.content.cloneNode(true));
+			this.addEventListener("click", event => {
+				var event = new Event("onClick");
+				this.dispatchEvent(event);
+			});
+			this._props = {};
+			var _selectedItem;
+
+		}
+        setSelected(newSelected) 
+        {
+            this._selectedItem = newSelected;
+            // fire "properties changed"
+            this.dispatchEvent(
+                new CustomEvent("propertiesChanged", 
+                    {
+                        detail: {
+                            properties: {
+                                            selectedItem: this._selectedItem
+                                        }
+                                }
+                    }
+                ))}
+
+        getSelected() {
+            return this._selectedItem;
+        }
+
+		onCustomWidgetBeforeUpdate(changedProperties) {
+			this._props = {
+				...this._props,
+				...changedProperties
+			};
+		}
+		onCustomWidgetAfterUpdate(changedProperties) {
+			if ("addUrl" in changedProperties) {
+				var table = this.shadowRoot.getElementById("myTable");
+				var row = table.insertRow(0);
+				var cell1 = row.insertCell(0);
+				cell1.innerHTML = '<a href="' + changedProperties["addUrl"].split('|')[1] + '">' + changedProperties["addUrl"].split('|')[0] + '</a>';
+				cell1.addEventListener("click", () => {
+					this._selectedItem = changedProperties["addUrl"].split('|')[0];
+				});
+			}
+			if ("color" in changedProperties) {
+				this.style["background-color"] = changedProperties["color"];
+			}
+			if ("opacity" in changedProperties) {
+				this.style["opacity"] = changedProperties["opacity"];
+			}
+		}
+	}
+	customElements.define("com-sample-box", Box);
 })();
