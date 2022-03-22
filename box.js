67329d396a3aa77e64fad815dbd33d9ea38054f2
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2022-03-22 07:56:41 -0700

    Update box.js

--- box.js
+++ box.js
@@ -46,8 +46,7 @@
     var cell1 = row.insertCell(0);
     cell1.innerHTML=  '<a href="'+ changedProperties["addUrl"].split('|')[1] +'">'+ changedProperties["addUrl"].split('|')[0] +'</a>';
 cell1.addEventListener("click",() => {
-	console.log(this._props['color']);
-    alert(changedProperties["addUrl"].split('|')[0]);
+	this._props['selectedItem']=changedProperties["addUrl"].split('|')[0];
 });	 
  }  
  if ("color" in changedProperties) {
