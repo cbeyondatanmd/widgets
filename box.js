a625a978ae43429f86f1fd4a877c3247a0034e56
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2022-03-22 17:12:05 -0700

    Update box.js

--- box.js
+++ box.js
@@ -13,6 +13,16 @@
     background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA0NDggNTEyIj48IS0tISBGb250IEF3ZXNvbWUgRnJlZSA2LjEuMSBieSBAZm9udGF3ZXNvbWUgLSBodHRwczovL2ZvbnRhd2Vzb21lLmNvbSBMaWNlbnNlIC0gaHR0cHM6Ly9mb250YXdlc29tZS5jb20vbGljZW5zZS9mcmVlIChJY29uczogQ0MgQlkgNC4wLCBGb250czogU0lMIE9GTCAxLjEsIENvZGU6IE1JVCBMaWNlbnNlKSBDb3B5cmlnaHQgMjAyMiBGb250aWNvbnMsIEluYy4gLS0+PHBhdGggZD0iTTIyNCAyNTZjNzAuNyAwIDEyOC01Ny4zMSAxMjgtMTI4cy01Ny4zLTEyOC0xMjgtMTI4QzE1My4zIDAgOTYgNTcuMzEgOTYgMTI4UzE1My4zIDI1NiAyMjQgMjU2ek0yNzQuNyAzMDRIMTczLjNDNzcuNjEgMzA0IDAgMzgxLjYgMCA0NzcuM2MwIDE5LjE0IDE1LjUyIDM0LjY3IDM0LjY2IDM0LjY3aDM3OC43QzQzMi41IDUxMiA0NDggNDk2LjUgNDQ4IDQ3Ny4zQzQ0OCAzODEuNiAzNzAuNCAzMDQgMjc0LjcgMzA0eiIvPjwvc3ZnPg==)
 }
 
+.icon-orange { 
+  width: 22px;
+  height: 22px;
+  display: inline-block;
+  background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/18515/heart.svg) no-repeat 50% 50%; 
+  background-size: cover;
+  -webkit-filter: hue-rotate(40deg) saturate(0.5) brightness(390%) saturate(4); 
+  filter: hue-rotate(40deg) saturate(0.5) brightness(390%) saturate(4); 
+}
+
 table, tr, td {
 
 	background-color:rgb(41, 49, 58);	   
@@ -59,7 +69,7 @@ a:active {
 </tr>
 <tr>
 <td>
-<a href="/sap/fpa/ui/app.html#/analyticapp&/aa/1D014E0008045C61D98AAEB6273E52D0"><i class="user-icon"></i>&nbsp;&nbsp;DASHBOARD&nbsp;&nbsp;</a>
+<a href="/sap/fpa/ui/app.html#/analyticapp&/aa/1D014E0008045C61D98AAEB6273E52D0"><i class="icon-orange"></i>&nbsp;&nbsp;DASHBOARD&nbsp;&nbsp;</a>
 </td>
 </tr>
 </table>
