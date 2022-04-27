c784c797270b1434ff3d121b73c33b01ce035604
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2022-04-27 11:37:11 -0700

    Update box.js

--- box.js
+++ box.js
@@ -138,7 +138,7 @@ td:active {
 			cell.addEventListener("click", () => {
 				this._selectedItem = caption;
 			});			
-            console.log(this.parentNode);
+            console.log(this.getRootNode);
 	}
 
 		addNavigation(caption, icon) {
