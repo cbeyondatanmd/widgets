ad5a45797bc5220a3c38ff6c8c632db951a070cc
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2022-04-27 16:59:16 -0700

    Update box.js

--- box.js
+++ box.js
@@ -139,7 +139,8 @@ td:active {
 				this._selectedItem = caption;
 			});
 
-			document.querySelector("iframe").contentWindow.document.querySelector("#__table2-reactTableContainer > div > div > div:nth-child(1) > div.htmlTableWrapper > div.freezeSectiontopRight > div.tableDivTable.reactTable > div:nth-child(1) > div.tableCell.cell2d47891b98.tableDivCell.headerCell.Categorynone.headerCell.colDimMember.dimensionMember > span")
+			debug.log(document.querySelector("iframe").contentWindow.document.querySelector("#__table2-reactTableContainer > div > div > div:nth-child(1) > div.htmlTableWrapper > div.freezeSectiontopRight > div.tableDivTable.reactTable > div:nth-child(1) > div.tableCell.cell2d47891b98.tableDivCell.headerCell.Categorynone.headerCell.colDimMember.dimensionMember > span"));
+			debug.log(document.querySelector("iframe"));
 		}
 		addNavigation(caption, icon) {
 			var table = this.shadowRoot.getElementById("tableNavigation");
