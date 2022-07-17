90045965121be750cb7ccbd9147a1a710ee6b31e
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2022-07-17 14:28:12 -0700

    Update tools.js

--- tools.js
+++ tools.js
@@ -24,22 +24,40 @@
 		}
 
 		postMessage(url, body, csrf) {
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
+			var xhr = new XMLHttpRequest();
+			xhr.withCredentials = true;
 
-xhr.addEventListener("readystatechange", function() {
-  if(this.readyState === 4) {
-    console.log(this.responseText);
-  }
-});
+			xhr.addEventListener("readystatechange", function() {
+			  if(this.readyState === 4) {
+			    console.log(this.responseText);
+			  }
+			});
 
-xhr.open("GET", "https://cbeyondata.us10.hcs.cloud.sap/sap/fpa/services/rest/epm/session?action=logon",false);
-// WARNING: Cookies will be stripped away by the browser before sending the request.
-xhr.setRequestHeader("X-CSRF-Token", "Fetch");
+			xhr.open("GET", "https://cbeyondata.us10.hcs.cloud.sap/sap/fpa/services/rest/epm/session?action=logon",false);
+			// WARNING: Cookies will be stripped away by the browser before sending the request.
+			xhr.setRequestHeader("X-CSRF-Token", "Fetch");
 
-xhr.send();
+			xhr.send();
+			
+			csrf = xhr.getResponseHeader("x-csrf-token");
+			
+			xhr = new XMLHttpRequest();
+			xhr.withCredentials = true;
 
-			return xhr.getResponseHeader("x-csrf-token");
+			xhr.addEventListener("readystatechange", function() {
+			  if(this.readyState === 4) {
+			    console.log(this.responseText);
+			  }
+			});
+
+			xhr.open("POST", "https://cbeyondata.us10.hcs.cloud.sap/sap/fpa/services/rest/fpa/collaboration?tenant=A",false);
+
+			xhr.setRequestHeader("X-CSRF-Token", csrf);
+			xhr.setRequestHeader("Content-Type", "text/plain");
+
+			xhr.send(data);
+
+			return xhr.responseText
             }	
         	
 
