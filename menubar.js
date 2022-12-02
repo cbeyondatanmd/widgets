a1fc8e0fc9a06307e4260bc6104c898c604e7c78
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2022-12-02 13:43:26 -0800

    Update menubar.js

--- menubar.js
+++ menubar.js
@@ -186,7 +186,7 @@ cell.innerHTML = '<a href="" onclick="return false;"><svg width="16" height="16"
 			cell.id = caption;
 
 
-cell.innerHTML = '<a href="" onclick="return false;"><svg class="child" width="16" height="16" viewBox="0 0 32 32"><path d="M30.148 5.588c-2.934-3.42-7.288-5.588-12.148-5.588-8.837 0-16 7.163-16 16s7.163 16 16 16c4.86 0 9.213-2.167 12.148-5.588l-10.148-10.412 10.148-10.412zM22 3.769c1.232 0 2.231 0.999 2.231 2.231s-0.999 2.231-2.231 2.231-2.231-0.999-2.231-2.231c0-1.232 0.999-2.231 2.231-2.231z"></path></svg><span class="childtext">&nbsp;&nbsp;' + caption + '&nbsp;&nbsp;</span></a>';	
+cell.innerHTML = '<a href="" onclick="return false;"><svg class="child" width="16" height="16" viewBox="0 0 32 32"><path d="'  +  icon + '"></path></svg><span class="childtext">&nbsp;&nbsp;' + caption + '&nbsp;&nbsp;</span></a>';	
 					
 
 			cell.addEventListener("click", () => {
