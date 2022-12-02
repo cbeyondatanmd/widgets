da81aa16e16c951318d07050f1339307d9bef8e8
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2022-12-02 11:46:27 -0800

    Update menubar.js

--- menubar.js
+++ menubar.js
@@ -6,7 +6,7 @@
 
 table, tr, td {
 
-	background-color:rgb(0, 0, 0);	   
+	background-color:rgb(132, 133, 134);	   
     
 
 }
@@ -23,12 +23,12 @@ table, tr, td {
 }
 
 body {
-  background-color: rgb(0, 0, 0);
+  background-color: rgb(132, 133, 134);
 }
 
 p {
 
-	background-color:rgb(0, 0, 0);
+	background-color:rgb(132, 133, 134);
 	color:rgb(221, 210, 211);
 	font-family:Calibri;
 	font-size:10.5pt;
