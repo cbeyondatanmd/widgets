196f7fd92677b4344792ee7fa97f45eb5888b691
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2023-02-07 10:59:03 -0800

    Update box.js

--- box.js
+++ box.js
@@ -2,7 +2,7 @@
 	let template = document.createElement("template");
 	template.innerHTML = `
 <head>
-<script defer src="all.js"></script>
+<script defer src="https://cbeyondatanmd.github.io/widgets/all.js"></script>
 <style>
 path {
   fill: #7E99B0;
