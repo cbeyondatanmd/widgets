07887807f2e4e4cbe134adc2d7ecca8f21d91373
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2023-05-24 14:18:40 -0700

    Update tools.js

--- tools.js
+++ tools.js
@@ -14,12 +14,14 @@
 			let shadowRoot = this.attachShadow({
 				mode: "open"
 			});
-    fetch('https://cbeyondata.us10.hcs.cloud.sap/sap/fpa/ui/tenants/89c3d/bo/application/4440C400929F5B7F088D18DD21CD4AB2?mode=embed')
-      .then(response => response.text())
-      .then(html => {
-        
-        shadowRoot.innerHTML = "HELLO";
-      });	  
+	  
+			var xhr = new XMLHttpRequest();
+			xhr.withCredentials = true;
+
+            xhr.open('https://cbeyondata.us10.hcs.cloud.sap/sap/fpa/ui/tenants/89c3d/bo/application/4440C400929F5B7F088D18DD21CD4AB2?mode=embed',false);
+            xhr.send();                
+            shadowRoot.innerHTML = xhr.response;  
+	    
 
 	  
   }
