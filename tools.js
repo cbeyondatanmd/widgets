2946a3629cd7a98a90cee3f876d1a97693609d9c
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2023-05-24 12:35:19 -0700

    Update tools.js

--- tools.js
+++ tools.js
@@ -11,7 +11,7 @@
 	class Tools extends HTMLElement {
   connectedCallback() {
     fetch('https://cbeyondata.us10.hcs.cloud.sap/sap/fpa/ui/tenants/89c3d/bo/application/4440C400929F5B7F088D18DD21CD4AB2?mode=edit')
-      .then(response => response.html())
+      .then(response => response.text())
       .then(html => {
         const shadow = this.attachShadow({ mode: 'closed' });
         shadow.innerHTML = html;
