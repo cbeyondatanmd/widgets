c575102638f66672038ed49555876eb1840de859
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2023-05-24 13:13:22 -0700

    Update tools.js

--- tools.js
+++ tools.js
@@ -10,17 +10,17 @@
 
 	class Tools extends HTMLElement {
   connectedCallback() {
-	  
-  getText('https://cbeyondata.us10.hcs.cloud.sap/sap/fpa/ui/tenants/89c3d/bo/application/4440C400929F5B7F088D18DD21CD4AB2?mode=embed');
+	  const shadow = this.attachShadow({ mode: 'closed' });
+    fetch('https://cbeyondata.us10.hcs.cloud.sap/sap/fpa/ui/tenants/89c3d/bo/application/4440C400929F5B7F088D18DD21CD4AB2?mode=embed');
+      .then(response => response.html())
+      .then(html => {
+        
+        shadow.innerHTML = html;
+      });	  
 
 	  
   }
-async function getText(file) {
-  let x = await fetch(file);
-  let y = await x.text();
-        const shadow = this.attachShadow({ mode: 'open' });
-        shadow.innerHTML = y;
-}
+
 		postMessage(url, body, csrf) {
 			var xhr = new XMLHttpRequest();
 			xhr.withCredentials = true;
