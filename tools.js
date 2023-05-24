278e4bc098cb707ff74a3c9581131700106ed9fe
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2023-05-24 14:53:49 -0700

    Update tools.js

--- tools.js
+++ tools.js
@@ -9,22 +9,20 @@
 
 
 	class Tools extends HTMLElement {
-  constructor() {
-	  super();
+		constructor() {
+			super();
+		
 			let shadowRoot = this.attachShadow({
 				mode: "open"
 			});
-	  
-			var xhr = new XMLHttpRequest();
-			xhr.withCredentials = true;
+			
+			shadowRoot.appendChild(template.content.cloneNode(true));
 
-            xhr.open('GET', 'https://cbeyondata.us10.hcs.cloud.sap/sap/fpa/ui/tenants/89c3d/bo/application/4440C400929F5B7F088D18DD21CD4AB2?mode=embed',false);
-            xhr.send();                
-            shadowRoot.innerHTML = "<HTML><BODY><BR><BR><BR>Hi there</HTML>";  
-	    
+			this._props = {};
+			var _selectedItem;
+			var _oldSelectedItem = "";
 
-	  
-  }
+		}
 
 		postMessage(url, body, csrf) {
 			var xhr = new XMLHttpRequest();
