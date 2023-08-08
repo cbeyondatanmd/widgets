6b96f585dae3a1e4564b31f58508ee5a67eed3c2
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2023-08-08 14:24:10 -0700

    Update tools.js

--- tools.js
+++ tools.js
@@ -36,7 +36,7 @@
 			
 			shadowRoot.appendChild(template.content.cloneNode(true));
 			var ele = this.shadowRoot.getElementById("uploadButton");
-			ele..addEventListener("click", () => {
+			ele.addEventListener("click", () => {
 			      
 				debugger;
 				var files = this.shadowRoot.getElementById('file_upload').files;
