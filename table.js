51e6f202bdac9034b8c884f421713c169a638f85
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2023-08-17 08:10:03 -0700

    Update table.js

--- table.js
+++ table.js
@@ -1,5 +1,6 @@
-let template = document.createElement("template");
-template.innerHTML = /*html*/ `
+(function() {
+	let template = document.createElement("template");
+	template.innerHTML =  `
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">  
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -34,3 +35,4 @@ class Table extends HTMLElement {
     }
 }
 window.customElements.define('com-cbeyondata-table', Table);
+})();
