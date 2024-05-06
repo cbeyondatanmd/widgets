f19d4ea24ec3539b6979c62503d69c6c8c20adb9
cbeyondatanmd <101596690+cbeyondatanmd@users.noreply.github.com>
2024-05-06 13:47:58 -0700

    Update content.js

--- content.js
+++ content.js
@@ -54,33 +54,7 @@
                 rtn = true;
                 iframe.style.visibility = "visible";
                 iframe.contentWindow.postMessage(filter, "*");
-                ///////
-                var shellContent = iframe.getElementById("shell_content");
-                var sapHcsShellMainContent = iframe.getElementsByClassName("sapHcsShellMainContent");
-                var sapHcsShellMainContainer = iframe.getElementsByClassName("sapHcsShellMainContainer");
-
-                if (shellContent !== undefined && shellContent !== null) {
-                    if (shellContent.classList.length > 0) {
-                        shellContent.classList.remove("sapMNav");
-                    }
-                }
-
-                if (sapHcsShellMainContent !== undefined && sapHcsShellMainContent !== null) {
-                    if (sapHcsShellMainContent.length > 0) {
-                        if (sapHcsShellMainContent[0].classList.length > 0) {
-                            sapHcsShellMainContent[0].classList.remove("sapHcsShellMainContent");
-                        }
-                    }
-                }
-
-                if (sapHcsShellMainContainer !== undefined && sapHcsShellMainContainer !== null) {
-                    if (sapHcsShellMainContainer.length > 0) {
-                        if (sapHcsShellMainContainer[0].classList.length > 0) {
-                            sapHcsShellMainContainer[0].classList.remove("sapHcsShellMainContainer");
-                        }
-                    }
-                }
-                /////////                
+            
             }
 
             return rtn;
